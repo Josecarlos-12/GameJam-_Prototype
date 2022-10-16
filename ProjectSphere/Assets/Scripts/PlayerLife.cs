36219a97cfac5d5528c53bf7723e80a1d55264ab
Marbin-Jeanpoll <marbin_26_06@hotmail.com>
2022-10-16 17:32:03 -0500

    Menu de derrota

--- ProjectSphere/Assets/Scripts/PlayerLife.cs
+++ ProjectSphere/Assets/Scripts/PlayerLife.cs
@@ -8,14 +8,22 @@ public class PlayerLife : MonoBehaviour
     public GameObject floor;
     public Scenary scenary;
     public int count;
+    public MeshRenderer sphere, box;
 
-    public GameObject jump,transformOn;
+    public GameObject jump, transformOn;
     public float reductionvelocity;
+    public ChangeCharacter death;
+    public bool deathPlayer;
+
     private void Update( )
     {
         if ( count>1)
         {
-            Destroy(gameObject);
+            deathPlayer = true;
+            Time.timeScale = 0;
+            //Destroy(gameObject);
+            sphere.material.color= Color.gray;
+            box.material.color= Color.gray;
         }
     }
 
@@ -32,6 +40,7 @@ public class PlayerLife : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Small"))
         {
+            death.death = true;
             count++;
             Debug.Log("smaal");
             floor.transform.position = new Vector3(0, 0, floor.transform.position.z + 4);
@@ -39,7 +48,11 @@ public class PlayerLife : MonoBehaviour
         }
         if (other.gameObject.CompareTag("Obstacle"))
         {
-            Destroy(gameObject);
+            deathPlayer = true;
+            death.death = true;
+            Time.timeScale = 0;
+            sphere.material.color = Color.gray;
+            box.material.color = Color.gray;
         }
         if (other.gameObject.name == "Saltar")
         {

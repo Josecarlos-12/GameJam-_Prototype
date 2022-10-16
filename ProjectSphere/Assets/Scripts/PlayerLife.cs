c107b0fb3c0ef1fc0d496df0c2a671c74a4bcc53
Marbin-Jeanpoll <marbin_26_06@hotmail.com>
2022-10-15 21:26:10 -0500

    daño de objeto pequeño

--- ProjectSphere/Assets/Scripts/PlayerLife.cs
+++ ProjectSphere/Assets/Scripts/PlayerLife.cs
@@ -5,19 +5,35 @@ using UnityEngine;
 public class PlayerLife : MonoBehaviour
 {
     public float life;
+    public GameObject floor;
+    public Scenary scenary;
+    public int count;
 
     private void Update( )
     {
-        if ( life <= 0 )
+        if ( count>1)
         {
             Destroy(gameObject);
         }
     }
-    private void OnCollisionEnter(Collision collision)
+
+     public void OnCollisionEnter(Collision collision)
     {
-        if ( collision.gameObject.CompareTag("Obstacle") )
+        if (collision.gameObject.CompareTag("Small"))
         {
-            life--;
+            Debug.Log("smaal"); 
+            floor.transform.position = new Vector3(0, 0, floor.transform.position.z-20);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Small"))
+        {
+            count++;
+            Debug.Log("smaal");
+            floor.transform.position = new Vector3(0, 0, floor.transform.position.z + 4);
+            scenary.speed = scenary.speed + 900;
         }
     }
 }
--- ProjectSphere/Assets/Scripts/Scenary/Scenary.cs
+++ ProjectSphere/Assets/Scripts/Scenary/Scenary.cs
@@ -11,7 +11,8 @@ public class Scenary : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        rb=GetComponent<Rigidbody>();
+        rb=GetComponent<Rigidbody>(); 
+        
     }
 
     // Update is called once per frame

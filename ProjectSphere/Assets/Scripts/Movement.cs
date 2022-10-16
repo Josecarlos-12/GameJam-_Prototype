c9322d5f35be0578ff2c22d0456c27f097b2f19f
Marbin-Jeanpoll <marbin_26_06@hotmail.com>
2022-10-15 20:07:04 -0500

    Salto mejorado

--- ProjectSphere/Assets/Scripts/Movement.cs
+++ ProjectSphere/Assets/Scripts/Movement.cs
@@ -2,46 +2,46 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float gravityF;
     public Rigidbody rb;
 
-    public float speedZ = 15f;
     public float sidewaysForce = 12f;
 
 
     public Vector3 inputMove;
-    // Update is called once per frame
-    void FixedUpdate()
+
+    public float jumpForce;
+    public bool canJump;
+
+    private void Update()
     {
-        rb.AddForce(transform.forward * speedZ);
 
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -4.3f, 4.3f), transform.position.y,transform.position.z);
-
-        rb.velocity =  transform.forward + inputMove.x * sidewaysForce * transform.right;
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -4.3f, 4.3f), transform.position.y, transform.position.z);
 
         inputMove.x = Input.GetAxis("Horizontal");
-        //Vector3 velocity = Vector3.zero;
-        if ( inputMove.x > 1 || inputMove.z > 1 )
-        {
-            
-            if ( this.gameObject.transform.position.x > LevelBoundary.leftSide )
-            {
-                rb.velocity =  transform.forward + transform.right * sidewaysForce * inputMove.x;
-                /*velocity.y = rb.velocity.y;
-                rb.velocity = velocity * gravityF;*/
-            }
+        rb.velocity = new Vector3(inputMove.x * sidewaysForce, rb.velocity.y);
 
-        }
-        if ( inputMove.x < 1 || inputMove.z < 1 )
+        if (Input.GetKeyDown(KeyCode.Space) && canJump)
         {
+            rb.AddForce(Vector3.up*jumpForce,ForceMode.Impulse);
             
-            if ( this.gameObject.transform.position.x < LevelBoundary.rightSide ) 
-            {
-                rb.velocity =  transform.forward + inputMove.x * sidewaysForce* transform.right;
-            }
-                
         }
-        //velocity.y += gravityF * Time.deltaTime;
-        
+
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            canJump = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            canJump = false;
+        }
+    }
+
 }
---
+++ ProjectSphere/Assets/Scripts/Scenary/Scenary.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEngine.EventSystems.StandaloneInputModule;
+
+public class Scenary : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb=GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector3(0, 0, speed * Time.deltaTime);
+    }
+}
